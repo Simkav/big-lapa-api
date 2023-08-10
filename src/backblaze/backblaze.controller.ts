bf0f6b482e39086f2be3200ce5de1f7ef977303e
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-08-10 21:21:44 +0300

    Add route to get by category document

--- src/backblaze/backblaze.controller.ts
+++ src/backblaze/backblaze.controller.ts
@@ -6,52 +6,54 @@ import {
   UseInterceptors,
   Res,
   BadRequestException,
+  Body,
   UploadedFile,
-} from '@nestjs/common'
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
-import { Response } from 'express'
-import * as mimeTypes from 'mime-types'
-import { BackblazeService } from './backblaze.service'
-import { ValidateUploadFiels } from './decorators/validate.upload.files.decorator'
-import { FileService } from '../files/file.service'
-import { extname } from 'path'
-import { ConfigService } from '@nestjs/config'
-import { IEnv } from 'src/configs/env.config'
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import * as mimeTypes from 'mime-types';
 
-const singleExtensionRegex = /^[^.]+\.[a-zA-Z0-9]+$/
+import { BackblazeService } from './backblaze.service';
+import { ValidateUploadFiels } from './decorators/validate.upload.files.decorator';
+import { FileService } from '../files/file.service';
+import { extname } from 'path';
+import { ConfigService } from '@nestjs/config';
+import { IEnv } from 'src/configs/env.config';
+
+const singleExtensionRegex = /^[^.]+\.[a-zA-Z0-9]+$/;
 
 @Controller('files')
 export class BackBlazeController {
-  constructor (
+  constructor(
     private readonly backblazeService: BackblazeService,
     private readonly fileService: FileService,
     private readonly configService: ConfigService<IEnv>,
   ) {}
   @Post('images')
   @UseInterceptors(FilesInterceptor('images', 6))
-  async uploadImage (
+  async uploadImage(
     @ValidateUploadFiels(2_097_152, 'image')
     images: Express.Multer.File[],
   ) {
     if (!images) {
-      throw new BadRequestException('No files uploaded')
+      throw new BadRequestException('No files uploaded');
     }
     const links = await Promise.all(
-      images.map(image =>
+      images.map((image) =>
         // this.fileService.convertToWebP(image.buffer, image.originalname),
         this.backblazeService.uploadFile(image.buffer, image.originalname),
       ),
-    )
-    return links
+    );
+    return links;
   }
 
   @Post('documents')
   @UseInterceptors(
     FileInterceptor('document', {
       fileFilter: (req, file, cb) => {
-        const allowedExtensions = ['.txt', '.pdf', '.doc']
-        const fileExtension = extname(file.originalname)
-        const mimeType = mimeTypes.lookup(fileExtension)
+        const allowedExtensions = ['.txt', '.pdf', '.doc'];
+        const fileExtension = extname(file.originalname);
+        const mimeType = mimeTypes.lookup(fileExtension);
 
         if (
           allowedExtensions.includes(fileExtension) &&
@@ -65,18 +67,18 @@ export class BackBlazeController {
                 'Invalid file name. Double extensions are not allowed.',
               ),
               false,
-            )
-            return
+            );
+            return;
           }
 
-          cb(null, true)
+          cb(null, true);
         } else {
           cb(
             new BadRequestException(
               'Only .txt, .doc, and .pdf files are allowed',
             ),
             false,
-          )
+          );
         }
       },
       limits: {
@@ -84,38 +86,39 @@ export class BackBlazeController {
       },
     }),
   )
-  async uploadDocument (@UploadedFile() document: Express.Multer.File) {
+  async uploadDocument(@UploadedFile() document: Express.Multer.File) {
     if (!document) {
-      throw new BadRequestException('No files uploaded')
+      throw new BadRequestException('No files uploaded');
     }
     const link = await this.backblazeService.uploadFile(
       document.buffer,
       document.originalname,
       'Document',
-    )
-    return link
+    );
+    return link;
   }
 
   @Get(':url')
-  async getFileByUrl (@Param('url') url: string, @Res() response: Response) {
-    response.setHeader('Content-type','image')
-    console.log(url)
-    const stream =  await this.backblazeService.getFile(url)
-    stream.on('data',data=>response.write(data))
-    stream.on('end',()=>{
-      response.end()
-    })
+  async getFileByUrl(@Param('url') url: string, @Res() response: Response) {
+    (await this.backblazeService.getFile(url)).pipe(response);
   }
 
   @Get('/document/:url')
-  async getDocumentByUrl (@Param('url') url: string, @Res() response: Response) {
-    const getFileName = await this.backblazeService.getFileInfo(url)
-    console.log(getFileName.name)
-    response.setHeader('Content-Type', 'application/octet-stream')
+  async getDocumentByUrl(@Param('url') url: string, @Res() response: Response) {
+    const getFileName = await this.backblazeService.getFileInfo(url);
+    console.log(getFileName.name);
+    response.setHeader('Content-Type', 'application/octet-stream');
     response.setHeader(
       'Content-Disposition',
       `attachment; filename=${encodeURI(getFileName.name)}`,
-    )
-    ;(await this.backblazeService.getFile(url)).pipe(response)
+    );
+    (await this.backblazeService.getFile(url)).pipe(response);
+  }
+
+  @Get('category/:category')
+  async getImagesByCategory(@Param('category') category: string) {
+    const images = await this.backblazeService.getImagesByCategory(category);
+    const imageUrls = images.map((image) => image.Url);
+    return imageUrls;
   }
 }
