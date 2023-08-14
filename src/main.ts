507c82594981a61f891c52eef7d60b7a4eab9ff6
Ivan Khomenko <simkav2411@gmail.com>
2023-08-14 20:16:17 +0300

    fix wrong properties in dtos

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const config = new DocumentBuilder()
     .setTitle('Big lapa')
     .setDescription('Big lapa API description')
