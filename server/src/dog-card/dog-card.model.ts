93a5dc219072665f335213634c1d878954563a4c
Ivan <46397494+Simkav@users.noreply.github.com>
2023-08-17 10:05:01 +0300

    Fix dog card (#13)
    * add validates to dog card model
    
    * refactor

--- server/src/dog-card/dog-card.model.ts
+++ server/src/dog-card/dog-card.model.ts
@@ -1,40 +1,50 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { prop } from '@typegoose/typegoose';
+import { IsArray, IsBoolean, IsString } from 'class-validator';
 
 export class DogCard {
   @ApiProperty()
   @prop({ required: true })
+  @IsString()
   mainPhoto: string;
 
   @ApiProperty()
   @prop({ required: true })
+  @IsArray()
   photos: string[];
 
   @ApiProperty()
+  @IsString()
   @prop({ required: true })
   name: string;
 
   @ApiProperty()
+  @IsString()
   @prop({ required: true })
   sex: string;
 
   @ApiProperty()
+  @IsString()
   @prop({ required: true })
   age: string;
 
   @ApiProperty()
+  @IsBoolean()
   @prop({ required: true })
   haschip: boolean;
 
   @ApiProperty()
+  @IsBoolean()
   @prop({ required: true })
   hasbreed: boolean;
 
   @ApiPropertyOptional()
+  @IsString()
   @prop()
   breed?: string;
 
   @ApiProperty()
+  @IsString()
   @prop({ required: true })
   size: string;
 }
