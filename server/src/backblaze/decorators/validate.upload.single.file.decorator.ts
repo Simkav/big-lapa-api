5185598c650295242c46b10497755ce99325bb0a
Ivan Khomenko <simkav2411@gmail.com>
2023-08-17 10:38:10 +0300

    fix single file upload

--- server/src/backblaze/decorators/validate.upload.single.file.decorator.ts
+++ server/src/backblaze/decorators/validate.upload.single.file.decorator.ts
@@ -3,20 +3,17 @@ import {
   ParseFilePipe,
   UploadedFile,
 } from '@nestjs/common';
-import { ParseFilesPipe } from '../pipes/parse-files.pipe';
 import { CustomFileTypeValidator } from '../custom-file-type-validator/CustomFileTypeValidator';
 
 export const ValidateUploadFile = (maxSize: number, fileExtensions: string[]) =>
   UploadedFile(
-    new ParseFilesPipe(
-      new ParseFilePipe({
-        fileIsRequired: true,
-        validators: [
-          new MaxFileSizeValidator({ maxSize }),
-          new CustomFileTypeValidator({
-            fileExtensions,
-          }),
-        ],
-      }),
-    ),
+    new ParseFilePipe({
+      fileIsRequired: true,
+      validators: [
+        new MaxFileSizeValidator({ maxSize }),
+        new CustomFileTypeValidator({
+          fileExtensions,
+        }),
+      ],
+    }),
   );
