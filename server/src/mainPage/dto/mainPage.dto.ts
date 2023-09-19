9054a50698f51d629bf4d14575a075bcc8b53ac9
Ivan <46397494+Simkav@users.noreply.github.com>
2023-09-19 19:23:58 +0300

    Feedback fix (#22)
    * add validate email decorator
    
    * add validate phone decorator
    
    * update dtos
    
    * update dto
    
    * update phone validation

--- server/src/mainPage/dto/mainPage.dto.ts
+++ server/src/mainPage/dto/mainPage.dto.ts
@@ -1,16 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
+import { ValidateEmail } from 'src/decorators/validate-email.decorator';
+import { ValidateNumber } from '../decorators/phone-validator.decorator';
 
 export class MainDto {
   @ApiProperty()
   @IsString()
+  @ValidateNumber()
   first_phoneNumber: string;
 
   @ApiProperty()
   @IsString()
+  @ValidateNumber()
   second_phoneNumber: string;
 
   @ApiProperty()
   @IsString()
+  @Length(6, 320)
+  @ValidateEmail()
+  @IsEmail()
   email: string;
 }
