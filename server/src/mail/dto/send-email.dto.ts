6ccd543687dda95b7f2c58b8bf4939ccc16e5945
Ivan Khomenko <simkav2411@gmail.com>
2023-09-19 19:21:11 +0300

    update dtos

--- server/src/mail/dto/send-email.dto.ts
+++ server/src/mail/dto/send-email.dto.ts
@@ -1,14 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
 import { IsNotRuEmail } from '../decorators/notRuEmail.decorator';
+import { ValidateEmail } from '../../decorators/validate-email.decorator';
 
 export class SendEmailDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @Length(2, 50)
+  @Matches(new RegExp(/^[a-zA-Z -]+$/gm), {
+    message: "Ім'я може складатись з літер пробілу або дефісу",
+  })
   name: string;
 
   @ApiProperty()
+  @Length(6, 320)
+  @ValidateEmail()
   @IsNotRuEmail({ message: 'Email with .ru domain is not allowed' })
   @IsEmail()
   email: string;
