c1c676a6802d89923ca27a37f22d04f17e929d56
Ivan Khomenko <simkav2411@gmail.com>
2023-09-19 19:20:46 +0300

    add validate phone decorator

---
+++ server/src/mainPage/decorators/phone-validator.decorator.ts
@@ -0,0 +1,40 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+const regexp = new RegExp(/^[0-9 ]$/gm);
+const splitedNumberLengths = [3, 2, 3, 4];
+
+@ValidatorConstraint({ async: true })
+export class ValidateNumberConstraint implements ValidatorConstraintInterface {
+  validate(number: string) {
+    if (!number) return false;
+    if (regexp.test(number)) {
+      return false;
+    }
+    const splitedNumbers = number.split(' ');
+    return splitedNumbers.every(
+      (numbers, i) => numbers.length === splitedNumberLengths[i],
+    );
+  }
+
+  defaultMessage() {
+    return 'Невірний формат телефону (380 67 568 1788) ';
+  }
+}
+
+export function ValidateNumber(validationOptions?: ValidationOptions) {
+  return (target: object, propertyName: string) => {
+    registerDecorator({
+      name: 'ValidateNumber',
+      target: target.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: ValidateNumberConstraint,
+    });
+  };
+}
